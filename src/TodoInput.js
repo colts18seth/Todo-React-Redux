682a4cf3c120feb9ccdbec1d6be7a2140efeb7ae
Seth <colts18seth@gmail.com>
2020-10-30 21:57:22 -0400

    added input styles

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,7 +3,12 @@ import './TodoInput.css';
 function TodoInput() {
     return (
         <div className="TodoInput">
-            <input id="TodoInput" placeholder="Add New Todo" />
+            <div className="input-group">
+                <input id="TodoInput" placeholder="Add New Todo" />
+                <div className="input-group-append">
+                    <button className="btn btn-info" type="button">Add</button>
+                </div>
+            </div>
         </div>
     );
 }
